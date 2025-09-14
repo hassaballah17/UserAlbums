6418939275662bbc104eb0ba4679984edb8f4830
Ahmed Hassaballah <ahmedhassaballah727@gmail.com>
2025-09-15 01:54:41 +0300

    Initial Commit

---
+++ UserAlbumsTests/UserAlbumsTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserAlbumsTests.swift
+//  UserAlbumsTests
+//
+//  Created by Ahmed Hassaballah on 15/09/2025.
+//
+
+import Testing
+@testable import UserAlbums
+
+struct UserAlbumsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
